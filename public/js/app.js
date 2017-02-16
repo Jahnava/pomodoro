0e3cd255eac9e1489cb3e6d66c8c6d879bb48c77
jahnava <jahnavalmt@gmail.com>
2017-02-15 20:58:36 -0500

    Accurately decreases minutes and seconds after start is clicked.

--- public/js/app.js
+++ public/js/app.js
@@ -1,32 +1,77 @@
 //Data and Variable Declirations
 var timer;
-var minutesLeft =25;
-var secondsLeft =0;
-var isOnBreak = false; //not currently on break
-var numberOfBreaks =0; //starting out having had 0 breaks so far
+var minutesLeft =10;
+//starting timer at 25 mins or 10 or whatever i want
+var secondsLeft =10;
+var isOnBreak = false;
+//not currently on break
+var numberOfBreaks =0;
+//starting out having had 0 breaks so far
 //Getting references to the DOM
 var minutes = document.querySelector('#minutes');
 var minutes = document.querySelector('#seconds');
-var startbutton = document.querySelector('#start');
+var startButton = document.querySelector('#start');
 
 //Initialization code
 //Event Listeners
-startButton.addeventlistener('click', start); //connects the start function to the start button by addeventlistener //
-
+startButton.addEventListener('click', start); //connects the start function to the start button by addingeventlistener
+render();
 
 //Function definition
-function start(){} //connected to start button
+ //connected to start button
 function start(){
   if(!timer){
-    timer=setInterval(tick,1000); //create timer if there is not already a timer
+    timer=setInterval(tick,1000); //create timer if there is not already a timer otherwise skip this
   }
 }
-//  setInterval(tick, 1000); //setting tick cycle every tick creates new frame //1000 milaseconds is equil to 1 second
-//}
-function tick(){} //does something in order over and over again
- console.log('tick'); //starting up the server to see if tick works
+function tick(){
+  decrementMinutes();
+  decrementSeconds();
+  render();
+}
+//set tick to run
+function decrementMinutes(){
+  if(secondsLeft=== 0){
+    minutesLeft-=1;
+  }
+}
+function decrementSeconds(){
+  if(secondsLeft === 0){
+    secondsLeft 59;
+} else {
+secondsLeft -=1;
+ }
+}
+//decreases mins and second after an interval
+function render(){
+minutes.textContent = pad(minutesLeft);
+console.log(secondsLeft);
+console.log(pad(secondsLeft));
+seconds.textContent = pad(secondsLeft);
+}
+//deturmines the amount of time that is left
 
-function render(){} //takes info and puts it in DOM
+function pad(num){
+  if(num < 10){
+    return `0${num}`;
+  } else {
+    return num;
+  }
+  //if its less than ten it spits out //num is a placeholder set to the word num
+}
+
+
+  //setInterval(tick, 1000); //setting tick cycle every tick creates new frame //1000 milaseconds is equil to 1 second
+//}
+//does something in order over and over again
+function tick(){
+ console.log('tick');
+ }
+ //starting up the server to see if tick works
+
+
+function render(){}
+//takes info and puts it in DOM
 
 
 
